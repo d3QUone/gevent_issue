0a2aec5c90e6850c63a978bfd8edf551b36a5356
Vladimir Kasatkin <vkasatkin@ivi.ru>
2020-06-11 19:08:31 +0300

    Add demo background worker

--- README.md
+++ README.md
@@ -32,3 +32,15 @@ Run with Docker:
 make build_app
 make run_app
 ```
+
+# Issues
+
+Two blocking issues:
+
+1) gevent has thread-specified errors: 
+
+https://github.com/gevent/gevent/issues/1437
+
+2) uwsgi can't run new gevent (with possible fix):
+
+https://github.com/unbit/uwsgi/issues/2177
--- src/main.py
+++ src/main.py
@@ -4,13 +4,36 @@
 
 # Step 2 - import all
 import threading
+import time
 
 from flask import Flask
 from flask import request
+from flask import current_app
+
+
+class BackgroundWorker(threading.Thread):
+
+    def __init__(self, _app):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+        self._app = _app
+        self.counter = 0
+
+    def run(self):
+        while True:
+            self.counter += 1
+
+            with self._app.app_context():
+                current_app.mega_counter = self.counter
+
+            time.sleep(5)
 
 
 app = Flask(__name__)
 app.debug = True
+
+BackgroundWorker(_app=app).start()
 
 
 @app.route('/')
@@ -22,5 +45,6 @@ def index_view():
 
 
 @app.route('/status')
-def index_view():
-    return 'Status OK'
+def status_view():
+    counter = getattr(current_app, 'mega_counter', None)
+    return 'counter = {}'.format(counter)
